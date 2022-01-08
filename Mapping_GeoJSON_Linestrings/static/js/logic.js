17ed52965e1def9473bc230888721728a6188e55
Falguni Mittal <mittalf@gmail.com>
2022-01-08 11:28:19 -0500

    Updated GeoJSON LineString file

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -38,13 +38,21 @@ L.control.layers(baseMaps).addTo(map);
 let torontoData = "https://raw.githubusercontent.com/mittalf/Mapping_Earthquakes/main/torontoRoutes.json";
 
 
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
 
 // Grabbing our GeoJSON data.
 d3.json(torontoData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data).addTo(map);
-});
+L.geoJSON(data, {
+  style: myStyle,
+  onEachFeature: function(feature, layer) {  
+    layer.bindPopup("<h3> Airlines: " + feature.properties.airline + "</h3> <hr> <h3> Destination: " + feature.properties.dst + "</h3>");
+  }
+}).addTo(map);
 
-
-
+})
